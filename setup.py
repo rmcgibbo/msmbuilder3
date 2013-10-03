0ac0edc7ff18039ba9a8055aee5d179eb2d9aa9e
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-02 23:55:52 -0700

    add minimal setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
- 
+"""MSMBuilder3: WIP
+
+Description
+"""
+
+DOCLINES = __doc__.split("\n")
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+##########################
+VERSION = "3.0.0"
+ISRELEASED = False
+__version__ = VERSION
+##########################
+
+setup(name='msmbuilder3',
+      author_email='rmcgibbo@gmail.com',
+      description=DOCLINES[0],
+      long_description="\n".join(DOCLINES[2:]),
+      version=__version__,
+      license='GPLv3+',
+      platforms=["Linux", "Mac OS-X", "Unix"],
+      packages=['msmbuilder3'],
+      package_dir={'msmbuilder3': 'msmbuilder'},
+      install_requires=['mdtraj'],
+      #scripts=['scripts/mdconvert', 'scripts/mdinspect'],
+)
