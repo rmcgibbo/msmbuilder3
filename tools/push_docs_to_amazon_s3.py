efeb8c038ab279cc1b3ea24d409f228385341891
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-09 16:11:21 -0700

    added script to push docs to s3

---
+++ tools/push_docs_to_amazon_s3.py
@@ -0,0 +1,24 @@
+import os
+import boto
+from boto.s3.key import Key
+import mdtraj
+
+# The secret key is available as a secure environment variable
+# on travis-ci to push the build documentation to Amazon S3.
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+BUCKET_NAME = 'msmbuilder3'
+ROOT = os.path.join(os.path.dirname(__file__), '../doc/_build')
+
+bucket_name = AWS_ACCESS_KEY_ID.lower() + '-' + BUCKET_NAME
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
+            AWS_SECRET_ACCESS_KEY)
+bucket = conn.create_bucket(BUCKET_NAME)
+
+for dirpath, dirnames, filenames in os.walk(ROOT):
+    for filename in filenames:
+        fn = os.path.join(dirpath, filename)
+        print('Uploading', fn, '...')
+        k = Key(bucket)
+        k.key = os.path.relpath(fn, root)
+        k.set_contents_from_filename(fn)
